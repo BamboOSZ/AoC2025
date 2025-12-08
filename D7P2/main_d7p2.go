cbf8cbeb7b1120a73f1798127a5a9e8a8c6fcbcd
Marcin Wojciechowski <marcin.wojciechowski@gmail.com>
2025-12-08 01:31:34 +0000

    Update Day 7 Part 2 implementation to read from the correct input file and enhance path counting logic
    - Changed input file from 'input_d7p1.txt' to 'input_d7p2.txt'.
    - Introduced a new paths matrix to track the number of paths at each position.
    - Updated logic to correctly propagate path counts based on conditions in the grid.
    - Adjusted output to display path counts alongside the grid representation.

--- D7P2/main_d7p2.go
+++ D7P2/main_d7p2.go
@@ -9,12 +9,13 @@ import (
 func main() {
 	fmt.Println("Day 7 Part 1")
 
-	input, err := os.ReadFile("input_d7p1.txt")
+	input, err := os.ReadFile("input_d7p2_s.txt")
 	if err != nil {
 		panic(err)
 	}
 
 	table := [][]byte{}
+	paths := [][]int{}
 
 	lines := strings.Split(string(input), "\n")
 
@@ -22,28 +23,50 @@ func main() {
 
 	for _, line := range lines {
 		table = append(table, []byte(line))
+		paths = append(paths, make([]int, len(line)))
 	}
 
 	for y := 0; y < len(table); y++ {
 		for x := 0; x < len(table[y]); x++ {
 			if table[y][x] == 'S' {
 				table[y+1][x] = '|'
+				paths[y+1][x] += 1
 			}
 
 			if y > 0 && table[y][x] == '.' && table[y-1][x] == '|' {
 				table[y][x] = '|'
+				paths[y][x] += paths[y-1][x]
 			}
 
 			if table[y][x] == '^' && table[y-1][x] == '|' {
+				paths[y][x] = paths[y-1][x]
 				table[y][x-1] = '|'
 				table[y][x+1] = '|'
-				total++
+
+				paths[y][x-1] += paths[y][x]
+				paths[y][x+1] += paths[y][x]
 			}
 		}
 	}
 
-	for _, line := range table {
-		fmt.Println(string(line))
+	for y := 0; y < len(table); y++ {
+		for x := 0; x < len(table[y]); x++ {
+			if table[y][x] == '.' {
+				fmt.Printf(" ")
+				continue
+			} else {
+				if table[y][x] != '|' {
+					fmt.Printf("%c", table[y][x])
+					continue
+				}
+				fmt.Printf("%d", paths[y][x])
+			}
+		}
+		fmt.Println()
+	}
+
+	for i := 0; i < len(paths[len(paths)-1]); i++ {
+		total += paths[len(paths)-1][i]
 	}
 
 	fmt.Println("Total: ", total)
